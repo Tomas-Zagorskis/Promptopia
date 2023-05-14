0e0bbdb7dfcfbd9d21ab2f24fe2b324de5e4053c
Tomas_Zagorskis <tomzagos@gmail.com>
2023-05-14 20:36:55 +0300

    feat: implement edit and delete prompt

--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -8,7 +8,7 @@ import Profile from '@components/Profile';
 
 const MyProfile = () => {
 	const { data: session } = useSession();
-
+	const router = useRouter();
 	const [posts, setPosts] = useState([]);
 
 	useEffect(() => {
@@ -21,9 +21,27 @@ const MyProfile = () => {
 		if (session?.user?.id) fetchPosts();
 	}, []);
 
-	const handleEdit = () => {};
+	const handleEdit = (post: any) => {
+		router.push(`/update-prompt?id=${post?._id}`);
+	};
 
-	const handleDelete = async () => {};
+	const handleDelete = async (post: any) => {
+		const hasConfirmed = confirm(
+			'Are you sure you want to delete this prompt?',
+		);
+		if (hasConfirmed) {
+			try {
+				await fetch(`api/prompt/${post._id.toString()}`, {
+					method: 'DELETE',
+				});
+
+				const filteredPosts = posts.filter((p: any) => p._id !== post._id);
+				setPosts(filteredPosts);
+			} catch (error) {
+				console.log(error);
+			}
+		}
+	};
 	return (
 		<Profile
 			name='My'
